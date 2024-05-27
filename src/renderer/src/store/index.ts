73ac754542882f9c92fcc31b5b9fc84e209eb9e0
G ARUTO <awdalig@gmail.com>
2024-05-27 20:50:12 -0300

    fixed windows background theme as there was compatability problems  with MacOS, created layouts for buttons to add and delete, reverted work that was meant for displaying notes in the list

---
+++ src/renderer/src/store/index.ts
@@ -0,0 +1,17 @@
+import {NoteInfo} from '@shared/models'
+
+export const notesMock: NoteInfo[] = [
+
+    {
+        title: 'Welcome',
+        lastEditTime: new Date().getTime()
+    },
+    {
+        title: 'notes1',
+        lastEditTime: new Date().getTime()
+    },
+    {
+        title: 'notes2',
+        lastEditTime: new Date().getTime()
+    }
+]
