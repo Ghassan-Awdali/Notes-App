fa3dfe05a2ae3e42b9b340ae4f5aa1bf2a04b8aa
G ARUTO <awdalig@gmail.com>
2024-05-28 21:07:58 -0300

    created a message displaying no notes when there are no notes added to the storage displaying on the sidebar in the notepreviewlist

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,8 +1,17 @@
 import React, { ComponentProps } from 'react'
 import {notesMock} from '@/store/mocks'
 import { NotePreview } from './NotePreview'
+import { twMerge } from 'tailwind-merge'
 
-export const NotePreviewList = ({...props}:ComponentProps<'ul'>) => {
+export const NotePreviewList = ({className, ...props}:ComponentProps<'ul'>) => {
+    if (notesMock.length === 0) {
+        return (
+          <ul className={twMerge('text-center pt-4', className)} {...props}>
+            <span>No Notes Yet!</span>
+          </ul>
+        )
+      }
+    
   return (
     <ul{...props}>{notesMock.map(note =>(
         <NotePreview key = {note.title + note.lastEditTime}{...note}></NotePreview>
